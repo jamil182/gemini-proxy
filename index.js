3db33d79e0f97edf82be6bfbf4ba258f86736a8b
jamil182 <jamilrivan@Gmail.com>
2026-02-21 00:10:27 +0700

    Remove comment in fetch function
    Removed comment about redirecting requests to the original Gemini server.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    // Mengalihkan semua permintaan ke server asli Gemini
     url.hostname = 'generativelanguage.googleapis.com';
     return fetch(new Request(url, request));
   }
